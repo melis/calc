5157ff43228c78afdc8a8a5108d5c8010baeb530
Melis <59087686+melis@users.noreply.github.com>
2021-09-10 20:23:18 +0600

    1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App container">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+      repudiandae facilis beatae quae odit dolorum laudantium est odio?
+      Similique enim ut est quas harum suscipit. Provident nemo quis accusamus
+      iure.
+    </div>
+  );
+}
+
+export default App;
